c512509cc53ab7e7772706f8c4342e7552a36a80
Tyler Dolezal <47109396+tylerdolezal@users.noreply.github.com>
2021-11-14 14:38:29 -0500

    Add files via upload

--- NEB-calcs/neb-calcs.py
+++ NEB-calcs/neb-calcs.py
@@ -22,56 +22,73 @@ Temps = np.arange(50,2.005e4,Tstep)
 Nsteps = int(1e6)
 N = len(energy)+1
 
-events = np.zeros((len(Temps),N))
+def MC_over_T(mc):
+    if mc:
 
-for k in range(len(Temps)):
-    kT = 0.025852*(Temps[k]/300) ; B = 1/kT
+        events = np.zeros((len(Temps),N))
 
-
-    memory = np.zeros((Nsteps,))
-    for event in range(Nsteps):
-        choice = int(uniform(0,len(energy))) ; E = energy[choice]
-        equation = exp(-B*E)
-        check = min(equation,1)
-        r = uniform(0,1)
-        if r < check:
-            memory[event] = choice+1
-        else:
-            memory[event] = 0
+        for k in range(len(Temps)):
+            kT = 0.025852*(Temps[k]/300) ; B = 1/kT
 
 
-
-    counts = np.zeros((N,))
-    x = np.arange(0,N)
-    for i in range(N):
-        count = 0
-        for event in memory:
-            if event == x[i]:
-                count +=1
-        counts[i] = count
-
-    print(Temps[k])
-
-    events[k] = counts/Nsteps
+            memory = np.zeros((Nsteps,))
+            for event in range(Nsteps):
+                choice = int(uniform(0,len(energy))) ; E = energy[choice]
+                equation = exp(-B*E)
+                check = min(equation,1)
+                r = uniform(0,1)
+                if r < check:
+                    memory[event] = choice+1
+                else:
+                    memory[event] = 0
 
 
-colors = ['#012A4A','#014F86','#2A6F97','#2C7DA0','#b5c99a','#468FAF','#61A5C2']
 
-c = np.arange(0,N)
-fig,ax=plt.subplots(figsize=(8,5))
-for i in range(len(Temps)):
-    for j in range(len(events[i])):
-        p = (-1*events[i]).argsort()
-        events[i] = events[i][p]
-        c = c[p]
-        plt.barh(Temps[i],width=events[i,j],height=Tstep,edgecolor="none",color=colors[c[j]])
+            counts = np.zeros((N,))
+            x = np.arange(0,N)
+            for i in range(N):
+                count = 0
+                for event in memory:
+                    if event == x[i]:
+                        count +=1
+                counts[i] = count
 
-ax.set_xticks(np.arange(0,np.max(events[-1])+0.10,0.10))
-ax.set_xticks(np.arange(0,np.max(events[-1])+0.05,0.05),minor=True)
-plt.xlim(0,np.max(events[-1])-0.05)
-plt.ylabel("Temperature (K)")
-plt.xlabel("Probability of Occurence (%)")
-plt.ylim(Temps[0],Temps[-1])
-plt.legend(["Clean subsurface","Set I", "Set I 1 NN","Set I 2 NN", "Set I 1 ML",
-            "Set II", "Set III"],loc="upper right",ncol=1)
-plt.savefig("mc-opT.png",dpi=600,bbox_inches='tight')
+            print(Temps[k])
+
+            events[k] = (counts/Nsteps)
+
+        np.savetxt("event-matrix.txt",events)
+
+    colors = ['#012A4A','#A9D6E5','#B7E4C7','#52B788','#2D6A4F','#61A5C2','#2A6F97']
+
+    events = np.loadtxt("event-matrix.txt")
+
+    c = np.arange(0,N)
+
+    fig,ax=plt.subplots(figsize=(8,5))
+
+    for i in range(len(Temps)):
+        for j in range(len(events[i])):
+            p = (-1*events[i]).argsort() # order max to min so tall bars do not cover
+                                         # up short bars
+            events[i] = events[i][p] # apply the ordering
+            c = c[p] # switch up the coloring same as we did the counts to maintain
+                     # consistency in coloring over all T
+
+            plt.barh(Temps[i],width=events[i,j],height=Tstep,edgecolor="none",color=colors[c[j]])
+
+    ax.set_xticks(np.arange(0,np.max(events[-1])+0.10,0.10))
+    ax.set_xticks(np.arange(0,np.max(events[-1])+0.05,0.05),minor=True)
+    ax.set_xticklabels(np.arange(0,100*np.max(events[-1])+10,10,dtype=int))
+    ax.tick_params(axis="x", bottom=True, top=True, labelbottom=True)
+    ax.tick_params(axis="x",which="minor", bottom=True, top=True, labelbottom=True)
+    plt.xlim(0,np.max(events[-1])-0.05)
+    plt.ylabel("Temperature (K)")
+    plt.xlabel("Probability of Occurence (%)")
+    plt.ylim(Temps[0],Temps[-1])
+    plt.legend(["Clean subsurface","Set I", "Set I 1 NN","Set I 2 NN", "Set I 1 ML",
+                "Set II", "Set III"],loc="upper right",ncol=1)
+
+    plt.savefig("mc-opT.png",dpi=600,bbox_inches='tight')
+
+MC_over_T(False)
--- NEB-calcs/reax_vs_energy.py
+++ NEB-calcs/reax_vs_energy.py
@@ -15,11 +15,30 @@ for trial in trials:
         data[i] = np.asarray(lines[i].split()[1:3],dtype=float)
     if trial+1 == 1 or trial+1 == 4:
         plt.plot(data[:,0],data[:,1],color="b")
+
     else:
         plt.plot(data[:,0],data[:,1],color='k')
 
-    plt.xlabel("Reaction Coordinate")
-    plt.ylabel("Energy (eV)")
+for j in range(2):
+    with open("01.with{}N/spline.dat".format(j+1)) as f:
+        lines = f.readlines()
+        f.close()
+    data = np.zeros((len(lines),2))
+    for i in range(len(data)):
+        data[i] = np.asarray(lines[i].split()[1:3],dtype=float)
+    plt.plot(data[:,0],data[:,1],color="b")
+# plot 1 ML coverage
+with open("01.trial-1ML/spline.dat") as f:
+    lines = f.readlines()
+    f.close()
+data = np.zeros((len(lines),2))
+for i in range(len(data)):
+    data[i] = np.asarray(lines[i].split()[1:3],dtype=float)
+plt.plot(data[:,0],data[:,1],color="k")
+
+
+plt.xlabel("Reaction Coordinate")
+plt.ylabel("Energy (eV)")
 plt.legend(["Hollow-Bridge", "Hollow-Hollow"])
 plt.xlim(0,max(data[:,0]))
 plt.savefig("reax-v-en.png",dpi=300,bbox_inches='tight')
